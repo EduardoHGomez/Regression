b691e4149b7c29454b456a5b58e589a2c09b93e7
Eduardo <hurtado.josegomez@gmail.com>
2023-10-16 19:30:55 -0600

    Fitting data

--- multilinear_lab.py
+++ multilinear_lab.py
@@ -3,3 +3,19 @@ import numpy as np
 import matplotlib.pyplot as plt
 
 # House prediction
+X_train = np.array([[2104, 5, 1, 45], [1416, 3, 2, 40], [852, 2, 1, 35]])
+y_train = np.array([460, 232, 178])
+
+print(f"X Shape: {X_train.shape}, X Type:{type(X_train)})")
+print(X_train)
+print(f"y Shape: {y_train.shape}, y Type:{type(y_train)})")
+print(y_train)
+
+# We chose some data that is close to the fitting model
+b_init = 785.1811367994083
+w_init = np.array([ 0.39133535, 18.75376741, -53.36032453, -26.42131618])
+print(f"w_init shape: {w_init.shape}, b_init type: {type(b_init)}")
+
+# get a row from our training data
+x_vec = X_train[0,:]
+print(f"x_vec shape {x_vec.shape}, x_vec value: {x_vec}")
